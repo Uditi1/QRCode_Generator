d890ae1145a6617f350ba08166f0a1d9881e3af7
Uditi1 <udititiwari@gmail.com>
2023-06-29 15:11:18 +0530

    lastcommit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+// import qr from '../../public/qr-code.png'
 
 const Card = () => {
 
@@ -6,8 +7,23 @@ const [input, setInput] = useState("")
 const [qr, setQr] = useState()
 const [isloading, setIsLoading] = useState(false)
 
+const getQRCode = async (e) => {
+    e.preventDefault()
+
+    try {
+setIsLoading(true)
+const res = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${input}`)
+console.log(res)
+setQr(res.url)
+    }catch(error) {
+        console.log(error)
+    }finally{
+        setIsLoading(false)
+    }
+}
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={getQRCode}>
         <h1 className='title'>QR Code Generator</h1>
 
         <input 
@@ -21,7 +37,7 @@ const [isloading, setIsLoading] = useState(false)
 
         {isloading && <div className='loading'><span></span>Loading...</div>}
 
-        {!isloading && (qr ? <img className='qr_code' src='qr' alt='qr_code' /> : <div className='loading'>Generate Amazing QR Code for you & your friends!</div>)}
+        {!isloading && (qr ? <img className='qr_code' src='./qr-code.png' alt='qr_code' /> : <div className='loading'>Generate Amazing QR Code for you & your friends!</div>)}
 
         <input type='submit' className='submit' value="Generate QR Code" />
     </form>
