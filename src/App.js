f8661bcd3953b2a01c6f86ac1e943ca8bfc34d87
Uditi1 <udititiwari@gmail.com>
2023-06-29 12:32:16 +0530

    commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Card from './Components/Card';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Card />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = () => {
+
+const [input, setInput] = useState("")
+const [qr, setQr] = useState()
+const [isloading, setIsLoading] = useState(false)
+
   return (
-    <div>Card</div>
+    <form className='form'>
+        <h1 className='title'>QR Code Generator</h1>
+
+        <input 
+        type='text'
+        className='input'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        required
+        placeholder='Enter URL or Text...'
+        />
+
+        {isloading && <div className='loading'><span></span>Loading...</div>}
+    </form>
   )
 }
 
